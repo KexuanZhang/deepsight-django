cc6615fe877a4b5552f89dc2fb4ac5f979ca0783
Ray <ruizhe.huang@mail.utoronto.ca>
2025-07-28 11:08:56 -0400

    removed usefilelistSSE hook

--- frontend/src/features/notebook/components/sources/SourcesList.tsx
+++ frontend/src/features/notebook/components/sources/SourcesList.tsx
@@ -11,7 +11,6 @@ import { FileIcons } from "@/types";
 import { Source, FileMetadata, SourcesListProps, SourceItemProps } from "@/features/notebook/type";
 import { useFileUploadStatus } from "@/features/notebook/hooks/generation/useFileUploadStatus";
 import { useFileStatus } from "@/features/notebook/hooks/generation/useFileStatus";
-// import { useFileListSSE } from "@/features/notebook/hooks/data/useFileListSSE"; // Temporarily disabled
 import AddSourceModal from "./AddSourceModal";
 
 const fileIcons: FileIcons = {
